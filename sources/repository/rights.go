748a6a95e7fec72e744674de6aa51bc28f809beb
Pavel Erokhin <mairwunnx@gmail.com>
2025-06-20 22:54:26 +0300

    Добавлен новый файл rights.go с реализацией репозитория для управления правами пользователей, включая методы проверки, добавления и удаления прав.

---
+++ sources/repository/rights.go
@@ -0,0 +1,72 @@
+package repository
+
+import (
+	"errors"
+	"slices"
+	"strings"
+	"ximanager/sources/persistence/entities"
+	"ximanager/sources/tracing"
+)
+
+var ErrNothingChanged = errors.New("nothing changed")
+var ErrRightNotFound = errors.New("right not found")
+
+var AvailableRights = []string{"switch_mode", "edit_mode", "manage_users", "manage_context"}
+
+type RightsRepository struct {
+	users *UsersRepository
+}
+
+func NewRightsRepository(users *UsersRepository) *RightsRepository {
+	return &RightsRepository{users: users}
+}
+
+func (x *RightsRepository) IsUserHasRight(logger *tracing.Logger, user *entities.User, scope string) bool {
+	if !user.IsActive {
+		logger.E("User is not active, fallback to denied")
+		return false
+	}
+
+	scope = strings.ToLower(strings.TrimSpace(scope))
+	for _, right := range user.Rights {
+		if strings.ToLower(strings.TrimSpace(right)) == scope {
+			return true
+		}
+	}
+
+	logger.W("User has no right", tracing.Scope, scope)
+	return false
+}
+
+func (x *RightsRepository) AddRightForUser(logger *tracing.Logger, user *entities.User, scope string) (*entities.User, error) {
+	if !slices.Contains(AvailableRights, scope) {
+		logger.E("Right not found", tracing.Scope, scope)
+		return nil, ErrRightNotFound
+	}
+
+	if slices.Contains(user.Rights, scope) {
+		logger.E("Right already exists", tracing.Scope, scope)
+		return nil, ErrNothingChanged
+	}
+
+	user.Rights = append(user.Rights, scope)
+	return x.users.UpdateUser(logger, user)
+}
+
+func (x *RightsRepository) RemoveRightForUser(logger *tracing.Logger, user *entities.User, scope string) (*entities.User, error) {
+	if !slices.Contains(AvailableRights, scope) {
+		logger.E("Right not found", tracing.Scope, scope)
+		return nil, ErrRightNotFound
+	}
+
+	if !slices.Contains(user.Rights, scope) {
+		logger.E("Right can't be removed, because it's not present", tracing.Scope, scope)
+		return nil, ErrNothingChanged
+	}
+
+	user.Rights = slices.DeleteFunc(user.Rights, func(right string) bool {
+		return strings.ToLower(strings.TrimSpace(right)) == scope
+	})
+
+	return x.users.UpdateUser(logger, user)
+}
