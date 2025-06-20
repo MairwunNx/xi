d6c2e4d40f374d5a45033d7e1a6d2ffeaaf91f39
Pavel Erokhin <mairwunnx@gmail.com>
2025-06-20 22:55:21 +0300

    Добавлен новый файл cmdutils.go с реализацией методов для обработки команд и аргументов сообщений в Telegram боте.

---
+++ sources/telegram/cmdutils.go
@@ -0,0 +1,45 @@
+package telegram
+
+import (
+	"strings"
+	"ximanager/sources/texting"
+	"ximanager/sources/tracing"
+
+	"errors"
+
+	"github.com/alecthomas/kong"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func (x *TelegramHandler) GetRequestText(msg *tgbotapi.Message) string {
+	if msg.IsCommand() {
+		req := msg.CommandArguments()
+		if !msg.Chat.IsPrivate() && len(strings.TrimSpace(req)) == 0 {
+			return ""
+		}
+		return req
+	}
+	return msg.Text
+}
+
+func (x *TelegramHandler) ParseCmd(cmd interface{}, args string) (*kong.Context, error) {
+	parser, err := kong.New(cmd)
+	if err != nil {
+		return nil, err
+	}
+	return parser.Parse(texting.ParseCmdArgs(args))
+}
+
+func (x *TelegramHandler) ParseKongCommand(log *tracing.Logger, msg *tgbotapi.Message, cmd interface{}) (*kong.Context, error) {
+	args := msg.CommandArguments()
+	if args == "" {
+		return nil, errors.New("command arguments are empty")
+	}
+
+	ctx, err := x.ParseCmd(cmd, args)
+	if err != nil {
+		log.W("Error parsing command", tracing.InnerError, err)
+		return nil, err
+	}
+	return ctx, nil
+}
