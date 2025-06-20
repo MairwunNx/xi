28dd04ef6b8283a7f1e265dc19952c0f5077e6a7
Pavel Erokhin <mairwunnx@gmail.com>
2025-06-20 22:53:26 +0300

    Добавлен новый файл zygote.go с реализацией генератора GORM для создания DAO и моделей на основе сущностей User, Donation, Message, Mode и SelectedMode.

---
+++ sources/persistence/gormgen/zygote.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"ximanager/sources/persistence/entities"
+
+	"gorm.io/gen"
+)
+
+func main() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath:      "./sources/persistence/gormdao/query",
+		ModelPkgPath: "./sources/persistence/gormdao/model",
+		Mode:         gen.WithDefaultQuery | gen.WithQueryInterface,
+	})
+
+	g.ApplyBasic(entities.User{}, entities.Donation{}, entities.Message{}, entities.Mode{}, entities.SelectedMode{})
+	g.Execute()
+}
