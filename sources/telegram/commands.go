07b4e84000197be7e53f2e27fffbd49e12e2f5a9
Pavel Erokhin <mairwunnx@gmail.com>
2025-06-20 22:55:51 +0300

    Добавлен новый файл commands.go с реализацией методов обработки команд для Telegram бота, включая управление режимами, пользователями и пожертвованиями.

---
+++ sources/telegram/commands.go
@@ -0,0 +1,180 @@
+package telegram
+
+import (
+	"fmt"
+	"ximanager/sources/persistence/entities"
+	"ximanager/sources/texting"
+	"ximanager/sources/tracing"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func (x *TelegramHandler) HandleXiCommand(log *tracing.Logger, user *entities.User, msg *tgbotapi.Message) {
+	if !x.throttler.IsAllowed(msg.From.ID) {
+		log.W("User exceeded rate throttler")
+		x.diplomat.Reply(log, msg, texting.MsgThrottleExceeded)
+		return
+	}
+
+	if msg.Photo != nil && len(msg.Photo) > 0 {
+		x.XiCommandPhoto(log, msg)
+		return
+	}
+
+	x.XiCommandText(log, msg)
+}
+
+func (x *TelegramHandler) HandleModeCommand(log *tracing.Logger, user *entities.User, msg *tgbotapi.Message) {
+	args := msg.CommandArguments()
+	if args == "" {
+		if !x.rights.IsUserHasRight(log, user, "switch_mode") {
+			x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgModeNoAccess))
+			return
+		}
+		x.ModeCommandSwitch(log, user, msg)
+		return
+	}
+
+	var cmd ModeCmd
+	ctx, err := x.ParseKongCommand(log, msg, &cmd)
+	if err != nil {
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgModeHelpText))
+		return
+	}
+
+	switch ctx.Command() {
+	case "add <chatid> <type> <name> <prompt>":
+		if !x.rights.IsUserHasRight(log, user, "edit_mode") {
+			x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgModeModifyNoAccess))
+			return
+		}
+		x.ModeCommandAdd(log, user, msg, int64(cmd.Add.ChatID), cmd.Add.Type, cmd.Add.Name, cmd.Add.Prompt)
+	case "list <chatid>":
+		if !x.rights.IsUserHasRight(log, user, "switch_mode") {
+			x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgModeNoAccess))
+			return
+		}
+		x.ModeCommandList(log, msg, int64(cmd.List.ChatID))
+	case "disable <chatid> <type>":
+		if !x.rights.IsUserHasRight(log, user, "edit_mode") {
+			x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgModeModifyNoAccess))
+			return
+		}
+		x.ModeCommandDisable(log, msg, int64(cmd.Disable.ChatID), cmd.Disable.Type)
+	case "enable <chatid> <type>":
+		if !x.rights.IsUserHasRight(log, user, "edit_mode") {
+			x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgModeModifyNoAccess))
+			return
+		}
+		x.ModeCommandEnable(log, msg, int64(cmd.Enable.ChatID), cmd.Enable.Type)
+	case "delete <chatid> <type>":
+		if !x.rights.IsUserHasRight(log, user, "edit_mode") {
+			x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgModeModifyNoAccess))
+			return
+		}
+		x.ModeCommandDelete(log, msg, int64(cmd.Delete.ChatID), cmd.Delete.Type)
+	case "edit <chatid> <type> <prompt>":
+		if !x.rights.IsUserHasRight(log, user, "edit_mode") {
+			x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgModeModifyNoAccess))
+			return
+		}
+		x.ModeCommandEdit(log, msg, int64(cmd.Edit.ChatID), cmd.Edit.Type, cmd.Edit.Prompt)
+	default:
+		log.W("Unknown mode subcommand", tracing.InternalCommand, ctx.Command())
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgModeHelpText))
+	}
+}
+
+func (x *TelegramHandler) HandleUsersCommand(log *tracing.Logger, user *entities.User, msg *tgbotapi.Message) {
+	if !x.rights.IsUserHasRight(log, user, "manage_users") {
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgUsersNoAccess))
+		return
+	}
+
+	var cmd UsersCmd
+	ctx, err := x.ParseKongCommand(log, msg, &cmd)
+	if err != nil {
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgUsersHelpText))
+		return
+	}
+
+	switch ctx.Command() {
+	case "remove <username>":
+		x.UsersCommandRemove(log, msg, cmd.Remove.Username)
+	case "edit <username> <rights>":
+		x.UsersCommandEdit(log, msg, cmd.Edit.Username, cmd.Edit.Rights)
+	case "disable <username>":
+		x.UsersCommandDisable(log, msg, cmd.Disable.Username)
+	case "enable <username>":
+		x.UsersCommandEnable(log, msg, cmd.Enable.Username)
+	case "window <username> <limit>":
+		x.UsersCommandWindow(log, msg, cmd.Window.Username, cmd.Window.Limit)
+	case "stack <username> <enabled>":
+		x.UsersCommandStack(log, msg, cmd.Stack.Username, cmd.Stack.Enabled)
+	default:
+		log.W("Unknown users subcommand", tracing.InternalCommand, ctx.Command())
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgUsersUnknownCommand))
+	}
+}
+
+func (x *TelegramHandler) HandleDonationsCommand(log *tracing.Logger, user *entities.User, msg *tgbotapi.Message) {
+	if !x.rights.IsUserHasRight(log, user, "manage_users") {
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgDonationsNoAccess))
+		return
+	}
+
+	var cmd DonationsCmd
+	ctx, err := x.ParseKongCommand(log, msg, &cmd)
+	if err != nil {
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgDonationsHelpText))
+		return
+	}
+
+	switch ctx.Command() {
+	case "add <username> <sum>":
+		x.DonationsCommandAdd(log, msg, cmd.Add.Username, cmd.Add.Sum)
+	default:
+		log.W("Unknown donations subcommand", tracing.InternalCommand, ctx.Command())
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgDonationsUnknownCommand))
+	}
+}
+
+func (x *TelegramHandler) HandleThisCommand(log *tracing.Logger, user *entities.User, msg *tgbotapi.Message) {
+	x.diplomat.Reply(log, msg, texting.XiifyManual(fmt.Sprintf(texting.MsgThisInfo, user.UserID, *user.Fullname, *user.Username, user.ID, user.Rights, msg.Chat.ID, msg.Chat.Type, msg.Chat.Title)))
+}
+
+func (x *TelegramHandler) HandleStatsCommand(log *tracing.Logger, user *entities.User, msg *tgbotapi.Message) {
+	x.StatsCommand(log, user, msg)
+}
+
+func (x *TelegramHandler) HandleContextCommand(log *tracing.Logger, user *entities.User, msg *tgbotapi.Message) {
+	if msg.Chat.Type != "private" && !x.rights.IsUserHasRight(log, user, "manage_context") {
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgContextNoAccess))
+		return
+	}
+
+	var cmd ContextCmd
+	ctx, err := x.ParseKongCommand(log, msg, &cmd)
+	if err != nil {
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgContextHelpText))
+		return
+	}
+
+	switch ctx.Command() {
+	case "refresh", "refresh <chatid>":
+		var chatID int64
+		if cmd.Refresh.ChatID != nil {
+			chatID = int64(*cmd.Refresh.ChatID)
+		} else {
+			chatID = msg.Chat.ID
+		}
+		x.ContextCommandRefresh(log, msg, chatID)
+	default:
+		log.W("Unknown context subcommand", tracing.InternalCommand, ctx.Command())
+		x.diplomat.Reply(log, msg, texting.XiifyManual(texting.MsgContextHelpText))
+	}
+}
+
+func (x *TelegramHandler) HandleWtfCommand(log *tracing.Logger, user *entities.User, msg *tgbotapi.Message) {
+	x.WtfCommand(log, msg)
+}
