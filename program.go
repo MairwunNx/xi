3efcdc713cb8ac14bc0f7ff0b41f4488583891d3
Pavel Erokhin <mairwunnx@gmail.com>
2025-06-20 22:56:04 +0300

    Добавлен новый файл program.go с реализацией основной логики запуска Telegram бота Xi Manager, включая инициализацию модулей и обработку событий старта и остановки.

---
+++ program.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"ximanager/sources/artificial"
+	"ximanager/sources/balancer"
+	"ximanager/sources/external"
+	"ximanager/sources/persistence"
+	"ximanager/sources/repository"
+	"ximanager/sources/throttler"
+	"ximanager/sources/tracing"
+
+	"go.uber.org/fx"
+)
+
+var (
+	version = "0.0.0"
+	buildTime = "1970-01-01"
+)
+
+func main() {
+	fx.New(
+		tracing.Module,
+		external.Module,
+		persistence.Module,
+		repository.Module,
+		throttler.Module,
+		artificial.Module,
+		balancer.Module,
+	
+		fx.Invoke(func(lc fx.Lifecycle, log *tracing.Logger) {
+			lc.Append(fx.Hook{
+				OnStart: func(ctx context.Context) error {
+					log.I("Xi Manager started successfully", "version", version, "build_time", buildTime)
+					return nil
+				},
+				OnStop: func(ctx context.Context) error {
+					log.I("Xi Manager stopped", "version", version, "build_time", buildTime)
+					return nil
+				},
+			})
+		}),
+	).Run()
+}
