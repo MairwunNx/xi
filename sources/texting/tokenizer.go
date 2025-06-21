504172e712e6b88537a0fa7e8d5cab9acb550a21
Pavel Erokhin <mairwunnx@gmail.com>
2025-06-21 21:58:57 +0300

    Оптимизирована функция Tokens в tokenizer.go для улучшения обработки токенов, добавлено кэширование кодировки tiktoken.

--- sources/texting/tokenizer.go
+++ sources/texting/tokenizer.go
@@ -6,14 +6,9 @@ import (
 	"github.com/pkoukk/tiktoken-go"
 )
 
-func Tokens(log *tracing.Logger, text string) int {
-	tkm, err := tiktoken.GetEncoding("o200k_base")
-	if err != nil {
-		tokens := len(text) / 4
-		log.E("An error occurred while getting tiktoken encoding, fallback tokens used", tracing.AiTokens, tokens, tracing.InnerError, err)
-		return tokens
-	}
+var tkm, _ = tiktoken.GetEncoding("o200k_base")
 
+func Tokens(log *tracing.Logger, text string) int {
 	return tracing.ReportExecutionForRIn(log,
 		func() int { return len(tkm.Encode(text, nil, nil)) },
 		func(l *tracing.Logger, tokens int) { l.I("Tokens counted", tracing.AiTokens, tokens) },
