72cfbc402840c744e3c5e51ecb9eeb63fab14cae
Pavel Erokhin <mairwunnx@gmail.com>
2025-06-22 13:07:46 +0300

    Обновлены функции EscapeMarkdown и UnescapeSpecials в markdown.go для улучшения экранирования символов и обработки специальных символов. Изменено напоминание о пожертвовании в phrases.go для уточнения формата сообщения.

--- sources/texting/markdown.go
+++ sources/texting/markdown.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 )
 
-const escapable = "_[]()~>#+-=|{}.!"
+const escapable = "_[]()~>#+-={}.!"
 
 func EscapeMarkdown(input string) string {
 	var str strings.Builder
@@ -21,15 +21,18 @@ func EscapeMarkdown(input string) string {
 func UnescapeSpecials(input string) string {
 	result := input
 	
-	// Убираем экранирование для символа > после жирного текста
-	result = strings.ReplaceAll(result, "**\\>", "**>")
+	// Убираем экранирование звездочек внутри жирного текста *text \* text*
+	result = strings.ReplaceAll(result, "*\\*", "**")
 	
-	// Убираем экранирование звездочек внутри жирного текста **text \* text**
+	// Убираем экранирование звездочек внутри курсивного текста **text \* text**
 	result = strings.ReplaceAll(result, "**\\*", "***")
 	result = strings.ReplaceAll(result, "\\***", "***")
 	
+	// Убираем экранирование подчеркиваний внутри курсивного текста _text \_ text_
+	result = strings.ReplaceAll(result, "_\\_", "__")
+	
 	// Убираем экранирование подчеркиваний внутри подчеркнутого текста __text \_ text__
-	result = strings.ReplaceAll(result, "__\\_", "__")
+	result = strings.ReplaceAll(result, "__\\_", "___")
 	result = strings.ReplaceAll(result, "\\___", "___")
 	
 	// Убираем экранирование тильд внутри зачеркнутого текста ~text \~ text~
@@ -39,12 +42,17 @@ func UnescapeSpecials(input string) string {
 	result = strings.ReplaceAll(result, "||\\|", "|||")
 	result = strings.ReplaceAll(result, "\\|||", "|||")
 	
-	// Убираем экранирование @ внутри markdown конструкций (например **@username**)
+	// Убираем экранирование @ внутри markdown конструкций (например *@username*)
+	result = strings.ReplaceAll(result, "*\\@", "*@")
 	result = strings.ReplaceAll(result, "**\\@", "**@")
 	result = strings.ReplaceAll(result, "_\\@", "_@")
 	result = strings.ReplaceAll(result, "__\\@", "__@")
 	result = strings.ReplaceAll(result, "~\\@", "~@")
 	result = strings.ReplaceAll(result, "||\\@", "||@")
+
+	// Убираем экранирование для символа > после жирного текста
+	result = strings.ReplaceAll(result, "*\\>", "*>")
+	result = strings.ReplaceAll(result, "**\\>", "**>")
 	
 	return result
 }
