08edf729c1e68192b969f4d9b6e0bb65292aa5cc
Pavel Erokhin <mairwunnx@gmail.com>
2025-06-22 15:27:34 +0300

    Обновлена функция TrimEndingBold для добавления символов "||" в конце строки, что улучшает обработку жирного текста в markdown.

--- sources/texting/markdown.go
+++ sources/texting/markdown.go
@@ -362,5 +362,5 @@ func TrimSpecials(input string) string {
 }
 
 func TrimEndingBold(input string) string {
-	return strings.TrimSuffix(input, "**")
+	return strings.TrimSuffix(input, "**") + "||"
 }
