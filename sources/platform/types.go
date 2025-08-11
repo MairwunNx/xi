cf00fb57c68186cd33afc7d4ea4567dfd34872c0
Pavel Erokhin <mairwunnx@gmail.com>
2025-08-12 00:16:48 +0300

    Добавлены новые типы и константы в файл types.go для улучшения структуры данных: введены UserGrade и MessageRole, а также добавлена структура RedisMessage для хранения сообщений с ролями.

--- sources/platform/types.go
+++ sources/platform/types.go
@@ -1,3 +1,23 @@
 package platform
 
 type ChatID int64
+
+type UserGrade = string
+
+const (
+	GradeBronze UserGrade = "bronze"
+	GradeSilver UserGrade = "silver"
+	GradeGold   UserGrade = "gold"
+)
+
+type MessageRole = string
+
+const (
+	MessageRoleUser      MessageRole = "user"
+	MessageRoleAssistant MessageRole = "assistant"
+)
+
+type RedisMessage struct {
+	Role    MessageRole `json:"role"`
+	Content string      `json:"content"`
+}
