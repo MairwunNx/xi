93deda31f9d873038c3a1113e35427ab3702c74b
Pavel Erokhin <mairwunnx@gmail.com>
2025-08-12 00:12:31 +0300

    Обновлен модуль artificial: добавлены новые зависимости NewContextManager и NewUsageLimiter для улучшения управления контекстом и контроля использования функций бота.

--- sources/artificial/module.go
+++ sources/artificial/module.go
@@ -2,4 +2,17 @@ package artificial
 
 import "go.uber.org/fx"
 
-var Module = fx.Module("artificial", fx.Provide(NewAIConfig, NewOpenRouterClient, NewOpenAIClient, NewSpendingLimiter, NewDialer, NewWhisper, NewVision))
+var Module = fx.Module(
+	"artificial",
+	fx.Provide(
+		NewAIConfig,
+		NewOpenRouterClient,
+		NewOpenAIClient,
+		NewContextManager,
+		NewUsageLimiter,
+		NewSpendingLimiter,
+		NewDialer,
+		NewWhisper,
+		NewVision,
+	),
+)
