6e523d35d4a240f684bc1c28a43fe1224e72dfff
Pavel Erokhin <mairwunnx@gmail.com>
2025-09-22 00:14:36 +0300

    Удалён скрипт архивации .archive.sh, который использовался для создания ZIP-архива проекта Xi Manager. Это упрощает структуру проекта и устраняет неиспользуемый код.

--- .archive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "🚀 Начинаем архивацию Xi Manager..."
-
-git archive --format=zip --add-file .env --output ./ximanager.zip master
-zip -ur ./ximanager.zip .git/
-
-echo "✅ Архивация завершена!"
