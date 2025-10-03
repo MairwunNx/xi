22a7274e7fa216838915ce5e9848509f3b8df987
Pavel Erokhin <mairwunnx@gmail.com>
2025-10-03 14:46:48 +0300

    🗑️ Удалены неиспользуемые импорты и функция OpenRouterMessageStackFrom из openrouter.go.

--- sources/artificial/openrouter.go
+++ sources/artificial/openrouter.go
@@ -2,7 +2,6 @@ package artificial
 
 import (
 	"net/http"
-	"ximanager/sources/platform"
 
 	openrouter "github.com/revrost/go-openrouter"
 )
@@ -14,32 +13,4 @@ func NewOpenRouterClient(config *AIConfig, client *http.Client) *openrouter.Clie
 	clientConfig.HttpReferer = "https://github.com/mairwunnx/xi"
 
 	return openrouter.NewClientWithConfig(*clientConfig)
-}
-
-func OpenRouterMessageStackFrom(pairs []platform.RedisMessage) []openrouter.ChatCompletionMessage {
-	var messages []openrouter.ChatCompletionMessage
-
-	for i := len(pairs) - 1; i >= 0; i-- {
-		pair := pairs[i]
-
-		uc := pair.Content
-		if uc != "" {
-			messages = append(messages, openrouter.ChatCompletionMessage{
-				Role:    openrouter.ChatMessageRoleUser,
-				Content: openrouter.Content{Text: uc},
-			})
-		}
-
-		if pair.Role == platform.MessageRoleAssistant {
-			xc := pair.Content
-			if xc != "" {
-				messages = append(messages, openrouter.ChatCompletionMessage{
-					Role:    openrouter.ChatMessageRoleAssistant,
-					Content: openrouter.Content{Text: xc},
-				})
-			}
-		}
-	}
-
-	return messages
 }
