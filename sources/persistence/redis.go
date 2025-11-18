7501a5f309d34bbdbaf26598545a8c41b642958a
MairwunNx <personal@mairwunnx.com>
2025-11-19 00:58:20 +0300

    🔧 Обновить функцию NewRedis для использования новой структуры конфигурации

--- sources/persistence/redis.go
+++ sources/persistence/redis.go
@@ -2,18 +2,19 @@ package persistence
 
 import (
 	"strconv"
+	"ximanager/sources/configuration"
 	"ximanager/sources/tracing"
 
 	"github.com/redis/go-redis/v9"
 )
 
-func NewRedis(config *RedisConfig, log *tracing.Logger) *redis.Client {
+func NewRedis(config *configuration.Config, log *tracing.Logger) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:                  config.Host + ":" + strconv.Itoa(config.Port),
-		Password:              config.Password,
-		DB:                    config.DB,
-		MaxRetries:            config.MaxRetries,
-		DialTimeout:           config.DialTimeout,
+		Addr:                  config.Redis.Host + ":" + strconv.Itoa(config.Redis.Port),
+		Password:              config.Redis.Password,
+		DB:                    config.Redis.DB,
+		MaxRetries:            config.Redis.MaxRetries,
+		DialTimeout:           config.Redis.DialTimeout,
 		ContextTimeoutEnabled: true,
 	})
 
