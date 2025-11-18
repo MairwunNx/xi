af65064574e3bcf63724e89c8a079a3376db9954
MairwunNx <personal@mairwunnx.com>
2025-11-19 00:55:34 +0300

    🔧 Обновить функцию NewBotAPI для использования новой структуры конфигурации

--- sources/telegram/botapi.go
+++ sources/telegram/botapi.go
@@ -1,13 +1,14 @@
 package telegram
 
 import (
+	"ximanager/sources/configuration"
 	"ximanager/sources/tracing"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func NewBotAPI(log *tracing.Logger, config *BotConfig) *tgbotapi.BotAPI {
-	bot, err := tgbotapi.NewBotAPI(config.Token)
+func NewBotAPI(log *tracing.Logger, config *configuration.Config) *tgbotapi.BotAPI {
+	bot, err := tgbotapi.NewBotAPI(config.Telegram.BotToken)
 	if err != nil {
 		log.F("Failed to initialize telegram bot", tracing.InnerError, err)
 	} else {
