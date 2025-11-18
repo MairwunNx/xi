437a8507505f1dd20962e6a854a951e3b523b3a4
MairwunNx <personal@mairwunnx.com>
2025-11-18 15:19:33 +0300

    ✨ Реализовать новый класс XiMessenger с локализацией сообщений

--- sources/texting/xiifiers.go
+++ sources/texting/xiifiers.go
@@ -1,15 +1,31 @@
 package texting
 
-import "fmt"
+import (
+	"fmt"
+	"ximanager/sources/localization"
 
-func Xiify(text string) string {
-	return fmt.Sprintf("%s%s", MsgXiResponse, text)
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type XiMessenger struct {
+	localization *localization.LocalizationManager
 }
 
-func XiifyManual(text string) string {
-	return fmt.Sprintf("%s%s", MsgXiManualResponse, text)
+func NewXiMessenger(localization *localization.LocalizationManager) *XiMessenger {
+	return &XiMessenger{localization: localization}
 }
 
-func XiifyAudio(text string) string {
-	return fmt.Sprintf("%s%s", MsgAudioSuccess, text)
+func (x *XiMessenger) Xiify(msg *tgbotapi.Message, text string) string {
+	prefix := x.localization.LocalizeBy(msg, "MsgXiResponse", nil)
+	return fmt.Sprintf("%s%s", prefix, text)
+}
+
+func (x *XiMessenger) XiifyManual(msg *tgbotapi.Message, text string) string {
+	prefix := x.localization.LocalizeBy(msg, "MsgXiManualResponse", nil)
+	return fmt.Sprintf("%s%s", prefix, text)
+}
+
+func (x *XiMessenger) XiifyAudio(msg *tgbotapi.Message, text string) string {
+	prefix := x.localization.LocalizeBy(msg, "MsgAudioSuccess", nil)
+	return fmt.Sprintf("%s%s", prefix, text)
 }
