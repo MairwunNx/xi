af02684e2590ab768df51f2bd8260d4c4c03f99b
MairwunNx <personal@mairwunnx.com>
2025-11-19 01:10:08 +0300

    ✨ Добавить новые типы для модуля искусственного интеллекта

---
+++ sources/artificial/types.go
@@ -0,0 +1,15 @@
+package artificial
+
+type ModelMeta struct {
+	Name            string `json:"name"`
+	AAI             int    `json:"aai"`
+	InputPricePerM  string `json:"input_price_per_m"`
+	OutputPricePerM string `json:"output_price_per_m"`
+	CtxTokens       string `json:"ctx_tokens"`
+}
+
+type ContextLimits struct {
+	TTL         int
+	MaxMessages int
+	MaxTokens   int
+}
