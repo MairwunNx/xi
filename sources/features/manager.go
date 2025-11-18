3bb636c217e04af136db5ed2506d1eacbc47bb3d
MairwunNx <personal@mairwunnx.com>
2025-11-19 01:09:06 +0300

    🔧 Обновить функцию NewFeatureManager для использования новой структуры конфигурации

--- sources/features/manager.go
+++ sources/features/manager.go
@@ -3,13 +3,14 @@ package features
 import (
 	"context"
 	"time"
+	"ximanager/sources/configuration"
 	"ximanager/sources/tracing"
 
 	"github.com/Unleash/unleash-client-go/v4"
 )
 
 const (
-  FeatureLocalizationAuto        = "general/localization/auto"
+	FeatureLocalizationAuto        = "general/localization/auto"
 	FeatureMessageSummarization    = "dialer/context/message/summarization"
 	FeatureClusterSummarization    = "dialer/context/cluster/summarization"
 	FeatureResponseLengthDetection = "dialer/response/length-detection"
@@ -17,26 +18,25 @@ const (
 
 type FeatureManager struct {
 	client *unleash.Client
-	config *FeatureConfig
 	log    *tracing.Logger
 }
 
-func NewFeatureManager(config *FeatureConfig, log *tracing.Logger) (*FeatureManager, error) {
+func NewFeatureManager(config *configuration.Config, log *tracing.Logger) (*FeatureManager, error) {
 	client, err := unleash.NewClient(
-		unleash.WithUrl(config.UnleashAPIURL),
-		unleash.WithAppName(config.UnleashAppName),
-		unleash.WithInstanceId(config.UnleashInstanceID),
-		unleash.WithRefreshInterval(time.Duration(config.RefreshInterval)*time.Second),
+		unleash.WithUrl(config.Features.UnleashAPIURL),
+		unleash.WithAppName(config.Features.UnleashAppName),
+		unleash.WithInstanceId(config.Features.UnleashInstanceID),
+		unleash.WithRefreshInterval(time.Duration(config.Features.RefreshInterval)*time.Second),
 		unleash.WithListener(&unleashListener{log: log}),
 	)
-	
+
 	if err != nil {
 		log.E("Failed to initialize Unleash client", tracing.InnerError, err)
 		return nil, err
 	}
-	
-	log.I("Unleash client initialized successfully", "api_url", config.UnleashAPIURL, "app_name", config.UnleashAppName, "instance_id", config.UnleashInstanceID, "refresh_interval", config.RefreshInterval)
-	return &FeatureManager{client: client, config: config, log: log}, nil
+
+	log.I("Unleash client initialized successfully", "api_url", config.Features.UnleashAPIURL, "app_name", config.Features.UnleashAppName, "instance_id", config.Features.UnleashInstanceID, "refresh_interval", config.Features.RefreshInterval)
+	return &FeatureManager{client: client, log: log}, nil
 }
 
 func (x *FeatureManager) IsEnabled(featureName string) bool {
