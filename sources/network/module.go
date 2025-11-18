db7dcc26eeff1c3c2518bc46b870c91345aa044a
MairwunNx <personal@mairwunnx.com>
2025-11-19 00:56:24 +0300

    🔧 Обновить модуль сети для удаления функции NewProxyConfig

--- sources/network/module.go
+++ sources/network/module.go
@@ -2,4 +2,4 @@ package network
 
 import "go.uber.org/fx"
 
-var Module = fx.Module("network", fx.Provide(NewProxyConfig, NewProxyDialer, NewProxyClient))
+var Module = fx.Module("network", fx.Provide(NewProxyDialer, NewProxyClient))
