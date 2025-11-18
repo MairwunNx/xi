4337c008abe1f54123bd9efaa2f6e2e0791b2428
MairwunNx <personal@mairwunnx.com>
2025-11-19 01:10:26 +0300

    🔧 Обновить функцию NewOpenAIClient для использования новой структуры конфигурации

--- sources/artificial/openai.go
+++ sources/artificial/openai.go
@@ -2,12 +2,13 @@ package artificial
 
 import (
 	"net/http"
+	"ximanager/sources/configuration"
 
 	"github.com/sashabaranov/go-openai"
 )
 
-func NewOpenAIClient(client *http.Client, config *AIConfig) *openai.Client {
-	openaiConfig := openai.DefaultConfig(config.OpenAIToken)
+func NewOpenAIClient(client *http.Client, config *configuration.Config) *openai.Client {
+	openaiConfig := openai.DefaultConfig(config.AI.OpenAIToken)
 	openaiConfig.HTTPClient = client
 	return openai.NewClientWithConfig(openaiConfig)
 }
