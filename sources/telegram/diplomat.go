fb326b699aa7b40242da24e9be376e7e5371c6b9
MairwunNx <personal@mairwunnx.com>
2025-11-18 17:06:02 +0300

    🔧 Обновить обработку сообщений с использованием новых зависимостей и локализации

--- sources/telegram/diplomat.go
+++ sources/telegram/diplomat.go
@@ -2,19 +2,22 @@ package telegram
 
 import (
 	"strings"
+	"ximanager/sources/localization"
 	"ximanager/sources/platform"
 	"ximanager/sources/repository"
-	"ximanager/sources/texting"
+	"ximanager/sources/texting/markdown"
+	"ximanager/sources/texting/transform"
 	"ximanager/sources/tracing"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type Diplomat struct {
-	bot *tgbotapi.BotAPI
-	config *DiplomatConfig
-	users *repository.UsersRepository
+	bot       *tgbotapi.BotAPI
+	config    *DiplomatConfig
+	users     *repository.UsersRepository
 	donations *repository.DonationsRepository
+	localization *localization.LocalizationManager
 }
 
 func NewDiplomat(bot *tgbotapi.BotAPI, config *DiplomatConfig, users *repository.UsersRepository, donations *repository.DonationsRepository) *Diplomat {
@@ -22,47 +25,45 @@ func NewDiplomat(bot *tgbotapi.BotAPI, config *DiplomatConfig, users *repository
 }
 
 func (x *Diplomat) Reply(logger *tracing.Logger, msg *tgbotapi.Message, text string) {
-	tracing.ReportExecution(logger,
-		func() {
-			for _, chunk := range texting.Chunks(text, x.config.ChunkSize) {
-				chattable := tgbotapi.NewMessage(msg.Chat.ID, texting.EscapeMarkdownActor(chunk))
-				chattable.ReplyToMessageID = msg.MessageID
-				chattable.ParseMode = tgbotapi.ModeMarkdownV2
+  defer tracing.ProfilePoint(logger, "Diplomat reply completed", "diplomat.reply")()
 
-				if strings.HasPrefix(text, texting.MsgXiResponse) {
-					user, err := x.users.GetUserByEid(logger, msg.From.ID)
-					if err != nil {
-						logger.E("Failed to get user", tracing.InnerError, err)
-					} else {
-						grade, err := x.donations.GetUserGrade(logger, user)
-						if err != nil {
-							logger.E("Failed to get donations", tracing.InnerError, err)
-						} else {
-							if grade != platform.GradeGold && *user.Username != "mairwunnx" {
-								chattable.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
-									tgbotapi.NewInlineKeyboardRow(
-										tgbotapi.NewInlineKeyboardButtonURL("Поддержать проект ❤️‍🔥", "https://www.tbank.ru/cf/3uoCqIOiT8V"),
-									),
-								)
-							}
-						}
+	for _, chunk := range transform.Chunks(text, x.config.ChunkSize) {
+		chattable := tgbotapi.NewMessage(msg.Chat.ID, markdown.EscapeMarkdownActor(chunk))
+		chattable.ReplyToMessageID = msg.MessageID
+		chattable.ParseMode = tgbotapi.ModeMarkdownV2
+
+		if strings.HasPrefix(text, x.localization.LocalizeBy(msg, "MsgXiResponse")) {
+			user, err := x.users.GetUserByEid(logger, msg.From.ID)
+			if err != nil {
+				logger.E("Failed to get user", tracing.InnerError, err)
+			} else {
+				grade, err := x.donations.GetUserGrade(logger, user)
+				if err != nil {
+					logger.E("Failed to get donations", tracing.InnerError, err)
+				} else {
+					if grade != platform.GradeGold && *user.Username != "mairwunnx" {
+						chattable.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+							tgbotapi.NewInlineKeyboardRow(
+								tgbotapi.NewInlineKeyboardButtonURL(x.localization.LocalizeBy(msg, "MsgDonationsSupport"), "https://www.tbank.ru/cf/3uoCqIOiT8V"),
+							),
+						)
 					}
-				}
-
-				if _, err := x.bot.Send(chattable); err != nil {
-					logger.E("Message chunk sending error", tracing.InnerError, err)
-					emsg := tgbotapi.NewMessage(msg.Chat.ID, texting.EscapeMarkdownActor(texting.MsgXiError))
-					emsg.ReplyToMessageID = msg.MessageID
-					emsg.ParseMode = tgbotapi.ModeMarkdownV2
-
-					if _, err := x.bot.Send(emsg); err != nil {
-						logger.E("Failed to send fallback message", tracing.InnerError, err)
-					}
-					break
 				}
 			}
-		}, func(l *tracing.Logger) { l.I("Message sent") },
-	)
+		}
+
+		if _, err := x.bot.Send(chattable); err != nil {
+			logger.E("Message chunk sending error", tracing.InnerError, err)
+			emsg := tgbotapi.NewMessage(msg.Chat.ID, markdown.EscapeMarkdownActor(x.localization.LocalizeBy(msg, "MsgXiError")));
+			emsg.ReplyToMessageID = msg.MessageID
+			emsg.ParseMode = tgbotapi.ModeMarkdownV2
+
+			if _, err := x.bot.Send(emsg); err != nil {
+				logger.E("Failed to send fallback message", tracing.InnerError, err)
+			}
+			break
+		}
+	}
 }
 
 func (x *Diplomat) SendTyping(logger *tracing.Logger, chatID int64) {
