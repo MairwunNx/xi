9b9ac4723c7759a3022ad1c020b1285ea846bd9c
MairwunNx <personal@mairwunnx.com>
2025-11-19 01:10:18 +0300

    🔧 Обновить функцию NewOpenRouterClient для использования новой структуры конфигурации

--- sources/artificial/openrouter.go
+++ sources/artificial/openrouter.go
@@ -2,12 +2,13 @@ package artificial
 
 import (
 	"net/http"
+	"ximanager/sources/configuration"
 
 	openrouter "github.com/revrost/go-openrouter"
 )
 
-func NewOpenRouterClient(config *AIConfig, client *http.Client) *openrouter.Client {
-	clientConfig := openrouter.DefaultConfig(config.OpenRouterToken)
+func NewOpenRouterClient(config *configuration.Config, client *http.Client) *openrouter.Client {
+	clientConfig := openrouter.DefaultConfig(config.AI.OpenRouterToken)
 	clientConfig.HTTPClient = client
 	clientConfig.XTitle = "Xi Manager"
 	clientConfig.HttpReferer = "https://github.com/mairwunnx/xi"
