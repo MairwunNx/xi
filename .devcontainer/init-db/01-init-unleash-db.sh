c2344e2a1497aed93acfd9c2fe9199d398e7bc09
MairwunNx <personal@mairwunnx.com>
2025-11-26 01:37:16 +0300

    🔧 Обновить скрипт инициализации базы данных: изменить пользователя и предоставить права на базу данных unleash.

--- .devcontainer/init-db/01-init-unleash-db.sh
+++ .devcontainer/init-db/01-init-unleash-db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "ximanager" --dbname "ximanager" <<-EOSQL
     SELECT 'CREATE DATABASE unleash'
     WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'unleash')\gexec
-    GRANT ALL PRIVILEGES ON DATABASE unleash TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE unleash TO "ximanager";
 EOSQL
