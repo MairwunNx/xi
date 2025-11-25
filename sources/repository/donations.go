0ce65b98a775407f4ae8c87a42f2b668e8100c74
MairwunNx <personal@mairwunnx.com>
2025-11-25 14:48:09 +0300

    🗑️ Удалить неиспользуемые методы в репозитории пожертвований.

--- sources/repository/donations.go
+++ sources/repository/donations.go
@@ -8,8 +8,6 @@ import (
 	"ximanager/sources/persistence/gormdao/query"
 	"ximanager/sources/platform"
 	"ximanager/sources/tracing"
-
-	"github.com/shopspring/decimal"
 )
 
 type DonationsRepository struct{}
@@ -61,60 +59,4 @@ func (x *DonationsRepository) GetUserGrade(logger *tracing.Logger, user *entitie
 
 	logger.I("User grade inferred (old donation)", "internal_user_grade", platform.GradeSilver, "user_age_hours", userAge.Hours())
 	return platform.GradeSilver, nil
-}
-
-func (x *DonationsRepository) GetDonationsByUser(logger *tracing.Logger, user *entities.User) ([]*entities.Donation, error) {
-	defer tracing.ProfilePoint(logger, "Donations get donations by user completed", "repository.donations.get.donations.by.user", "user_id", user.ID)()
-  ctx, cancel := platform.ContextTimeoutVal(context.Background(), 20*time.Second)
-	defer cancel()
-
-	q := query.Q.WithContext(ctx)
-
-	donations, err := q.Donation.Where(query.Donation.User.Eq(user.ID)).Order(query.Donation.CreatedAt.Desc()).Find()
-	if err != nil {
-		logger.E("Failed to get donations", tracing.InnerError, err)
-		return nil, err
-	}
-
-	logger.I("Donations fetched")
-	return donations, nil
-}
-
-func (x *DonationsRepository) GetDonationsWithUsers(logger *tracing.Logger) ([]*entities.Donation, error) {
-	defer tracing.ProfilePoint(logger, "Donations get donations with users completed", "repository.donations.get.donations.with.users")()
-	ctx, cancel := platform.ContextTimeoutVal(context.Background(), 20*time.Second)
-	defer cancel()
-
-	q := query.Q.WithContext(ctx)
-
-	donations, err := q.Donation.Preload(query.Donation.UserEntity).Order(query.Donation.Sum.Desc()).Find()
-	if err != nil {
-		logger.E("Failed to get donations with users", tracing.InnerError, err)
-		return nil, err
-	}
-
-	logger.I("Donations with users fetched")
-	return donations, nil
-}
-
-func (x *DonationsRepository) CreateDonation(logger *tracing.Logger, user *entities.User, sum float64) (*entities.Donation, error) {
-	defer tracing.ProfilePoint(logger, "Donations create donation completed", "repository.donations.create.donation", "user_id", user.ID, "sum", sum)()
-  ctx, cancel := platform.ContextTimeoutVal(context.Background(), 20*time.Second)
-	defer cancel()
-
-	q := query.Q.WithContext(ctx)
-
-	donation := &entities.Donation{
-		User: user.ID,
-		Sum:  decimal.NewFromFloat(sum),
-	}
-
-	err := q.Donation.Create(donation)
-	if err != nil {
-		logger.E("Failed to create donation", tracing.InnerError, err)
-		return nil, err
-	}
-
-	logger.I("Created donation")
-	return donation, nil
 }
